dfe71bae847d8533c2ac0644ae5ae80a46221b74
思宏 <fubang.zfb@alibaba-inc.com>
2023-11-08 11:09:41 +0800

    init

--- rex/main.py
+++ rex/main.py
@@ -10,15 +10,16 @@ import jsonlines
 import torch
 import torch.distributed as dist
 
-from data_utils import data_loader, token_config
-from arguments import get_args, DataArguments, UIEArguments
-from model.model import RexModel
 from time import time
-# from model.USM import Model
-from Trainer.trainer import RexModelTrainer
-from Trainer.utils import compute_metrics
+
 
 def main():
+    from data_utils import data_loader, token_config
+    from arguments import get_args, DataArguments, UIEArguments
+    from model.model import RexModel
+    # from model.USM import Model
+    from Trainer.trainer import RexModelTrainer
+    from Trainer.utils import compute_metrics
     data_args, training_args, model_args = get_args()
     
     # training_args.output_dir = os.path.join(training_args.output_dir, training_args.run_name, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
